b32db60a742ae015328174419c338192215a9fa5
morteeke <maarten-r@outlook.com>
2023-04-12 19:57:12 +0200

    Update formulier.js
    -checked nu eerst of email leeg is. zo niet dan kijkt het na of het een correct email adres is.

--- scripts/formulier.js
+++ scripts/formulier.js
@@ -11,18 +11,29 @@ function validateForm(){
     checkEmptyField("land", "Het veld land is vereist.");
     checkEmptyField("provincie", "Het veld provincie is vereist.");
 
+    checkEmptyField("email", "Het veld email is vereist.");
+
+    alert(errors.join(', '));
+    errors = [];
 }
 
 function checkEmptyField(veld, melding){
     //We kijken na of de value van het veld empty is of whitespace. (whitespace kunnen we controleren met trim())
-    if(document.getElementById(veld).value.trim() == ""){
+    let elementValue = document.getElementById(veld).value;
+    if(elementValue.trim() == ""){
         errors[errors.length] = melding;
+    }
+
+    //validate email en als dit false is dan voegen we een error toe aan the error array.
+    else if(!validateEmail(elementValue) && veld == "email"){
+        errors[errors.length] = "E-mailadres is niet correct."; 
     }
 }
 
 function validateEmail(email){
-    //Hier gaan we de email valideren.
-    if(!email.indexOf('@')){
+
+    //Nakijken of de string een @ bevat.
+    if(email.indexOf('@') == -1){
         return false;
     }
 
