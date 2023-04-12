f00a56961e21e38cba94c960c05b8519173052dd
morteeke <maarten-r@outlook.com>
2023-04-12 19:01:15 +0200

    controle op velden
    checkEmptyField toegevoegd

---
+++ scripts/formulier.js
@@ -0,0 +1,29 @@
+document.getElementById("alert-column").style.display = "none";
+
+let errors = [];
+
+function validateForm(){
+    
+    checkEmptyField("voornaam", "Het veld voornaam is vereist.");
+    checkEmptyField("achternaam", "Het veld achternaam is vereist.")
+    checkEmptyField("gebruikersnaam", "Het veld gebruikersnaam is vereist.");
+    checkEmptyField("adres", "Het veld adres is vereist.");
+    checkEmptyField("land", "Het veld land is vereist.");
+    checkEmptyField("provincie", "Het veld provincie is vereist.");
+
+}
+
+function checkEmptyField(veld, melding){
+    //We kijken na of de value van het veld empty is of whitespace. (whitespace kunnen we controleren met trim())
+    if(document.getElementById(veld).value.trim() == ""){
+        errors[errors.length] = melding;
+    }
+}
+
+function validateEmail(email){
+    //Hier gaan we de email valideren.
+}
+
+function validatePassword(password){
+    //Controle wachtwoord.
+}
